b40ed8d8296eac7e3a9551905c45f12df59efffb
Konstantin <rybkin-k@mail.ru>
2017-07-07 03:03:07 +0700

    Somebody

--- bot.py
+++ bot.py
@@ -94,4 +94,4 @@ if __name__ == '__main__':
     bot.remove_webhook()
     bot.set_webhook(url=cfg.HOST, certificate=open(cfg.SSL_CERT, 'rb'))
 
-    app.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=cfg.PORT)
--- config.py
+++ config.py
@@ -4,7 +4,7 @@ STATS_URL = "https://abit.ifmo.ru/bachelor/statistics/applications/"
 ABIT_LIST_URL = "https://abit.ifmo.ru/bachelor/rating_rank/all/60/"
 
 HOST = 'https://ctifmobot.herokuapp.com/'
-PORT = 80
+PORT = 8443
 LISTEN = '0.0.0.0'
 
 SSL_CERT = './cert.pem'
