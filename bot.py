e178605e0f8caef12ba06f105807059ad25d2b28
Konstantin <rybkin-k@mail.ru>
2017-07-07 02:11:10 +0700

    add webhook server

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import telebot
 import strings
+import os
+import cherrypy
 import page_parser as pp
 import config as cfg
 
@@ -80,4 +82,32 @@ def handle_message(message):
     bot.send_message(message.chat.id, result_text)
 
 
-bot.polling()
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+                        'content-type' in cherrypy.request.headers and \
+                        cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
+
+
+bot.remove_webhook()
+
+bot.set_webhook(url=cfg.WEBHOOK_URL_BASE + cfg.WEBHOOK_URL_PATH,
+                certificate=open(cfg.WEBHOOK_SSL_CERT, 'r'))
+
+cherrypy.config.update({
+    'server.socket_host': cfg.WEBHOOK_LISTEN,
+    'server.socket_port': cfg.WEBHOOK_PORT,
+    'server.ssl_module': 'builtin',
+    'server.ssl_certificate': cfg.WEBHOOK_SSL_CERT,
+    'server.ssl_private_key': cfg.WEBHOOK_SSL_PRIV
+})
+
+cherrypy.quickstart(WebhookServer(), cfg.WEBHOOK_URL_PATH, {'/': {}})
--- config.py
+++ config.py
@@ -2,3 +2,13 @@ TOKEN = '433784852:AAEeqexG37bwUs15_2QbO3h2DEQB2E0sRuM'
 
 STATS_URL = "https://abit.ifmo.ru/bachelor/statistics/applications/"
 ABIT_LIST_URL = "https://abit.ifmo.ru/bachelor/rating_rank/all/60/"
+
+WEBHOOK_HOST = 'https://ctifmobot.herokuapp.com/'
+WEBHOOK_PORT = 443
+WEBHOOK_LISTEN = '0.0.0.0'
+
+WEBHOOK_SSL_CERT = './cert.pem'
+WEBHOOK_SSL_PRIV = './private.key'
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % TOKEN
