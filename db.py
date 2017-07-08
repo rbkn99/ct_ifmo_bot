c3b85792b32fc25a4e574428bc8434e2ba4760fa
Konstantin <rybkin-k@mail.ru>
2017-07-09 02:17:01 +0700

    ...

---
+++ db.py
@@ -0,0 +1,20 @@
+import os
+import psycopg2
+import urllib.parse as urlparse
+
+
+class Database:
+
+    def __init__(self):
+        urlparse.uses_netloc.append("postgres")
+        url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+        conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
+
+        self.cursor = conn.cursor()
