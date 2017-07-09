45ed2dc9e49c7f135aabfaecf449079b8d675fc3
Konstantin <rybkin-k@mail.ru>
2017-07-09 23:02:25 +0700

    ...

--- page_parser.py
+++ page_parser.py
@@ -58,9 +58,6 @@ def get_new_abits():
     new_abits = []
     for abit in abits_names:
         if abit not in db_abits_names:
-            print(1488)
-            print(abit)
-            print(search_abit(abit, abits))
-            new_abits.append(search_abit(abit, abits)[0])
+            new_abits.append(search_abit(abit.split(' '), abits)[0])
     print(104)
     return new_abits
